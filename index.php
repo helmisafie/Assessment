18afcc89003b050e6c14e939bf3628204f47aa78
helmisafie <helmicrutchlow@gmail.com>
2023-06-18 19:53:01 +0800

    Correction

--- index.php
+++ index.php
@@ -11,64 +11,84 @@
     <div class="container">
         <h1>Calculate</h1>
         <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-        <div class="form-group py-2">
-            <label for="InputVoltage">Voltage</label>
-            <input type="number" class="form-control" name="InputVoltage" step="any">
-            <small id="" class="form-text">Voltage (V)</small>
-        </div>
-        <div class="form-group py-2">
-            <label for="InputCurrent">Current</label>
-            <input type="number" class="form-control" name="InputCurrent" step="any">
-            <small id="" class="form-text">Ampere (A)</small>
-        </div>
-        <div class="form-group py-2">
-            <label for="InputCurrentRate">Current Rate</label>
-            <input type="number" class="form-control" name="InputCurrentRate" step="any">
-            <small id="" class="form-text">sen/kWh</small>
-        </div>
-        <button type="submit" class="btn btn-primary p-2">Calculate</button>
-    </form>
-    <?php
+            <div class="form-group py-2">
+                <label for="InputVoltage">Voltage</label>
+                <input type="number" class="form-control" name="InputVoltage" step="any">
+                <small id="" class="form-text">Voltage (V)</small>
+            </div>
+            <div class="form-group py-2">
+                <label for="InputCurrent">Current</label>
+                <input type="number" class="form-control" name="InputCurrent" step="any">
+                <small id="" class="form-text">Ampere (A)</small>
+            </div>
+            <div class="form-group py-2">
+                <label for="InputCurrentRate">Current Rate</label>
+                <input type="number" class="form-control" name="InputCurrentRate" step="any">
+                <small id="" class="form-text">sen/kWh</small>
+            </div>
+            <button type="submit" class="btn btn-primary p-2">Calculate</button>
+        </form>
 
-        function calculateRates($voltage, $current, $currentRate)
+        <?php
+
+        function calculatePower($voltage, $current)
         {
-
-            
-            echo '<table class="table">';
-            echo '<tr><th><b>#<th>Hour</th><th>Energy (kWh)</th><th>TOTAL (RM)</th></tr>';
-            // Calculations
-            
-            for ($hours = 1; $hours<=24; $hours++) {
-                $power = ($voltage * $current)/1000;
-                $energy = ($power * $hours * 1000)/1000;
-                $total = $energy * ($currentRate/100);
-
-                echo '<tr>';
-                echo '<td><b>' . $hours . '</b></td>';
-                echo '<td>' . $hours . '</td>';
-                echo '<td>' . number_format($energy,5) . '</td>';
-                echo '<td>' . (round($total,2) . '</td>');
-                echo '</tr>';
-            }
-            
-            echo '<div class="card p-4 mt-4 border-primary mb-3 style="max-width: 18rem "><h2>Power =' . number_format($power,5);
-            echo '<br><h2>Rate: ' . number_format($currentRate,2); '</div>';
-            echo '</table>';
-           
+            $power = $voltage * $current * 0.001; 
+            return $power;
         }
 
-        // Check if the form is submitted
+        function calculateEnergy($power, $hour)
+        {
+            $energy = $power * $hour;
+            return $energy;
+        }
+
+        function calculateTotalCharge($energy, $currentRate)
+        {
+            $totalCharge = $energy * ($currentRate / 100);
+            return $totalCharge;
+        }
+
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            // Get user input
-            $voltage = $_POST['InputVoltage']; // in volts
-            $current = $_POST['InputCurrent']; // in amperes
-            $currentRate = $_POST['InputCurrentRate']; // rate in percentage
+            $voltage = $_POST['InputVoltage']; 
+            $current = $_POST['InputCurrent']; 
+            $currentRate = $_POST['InputCurrentRate']; 
 
-            // Call the function
-            calculateRates($voltage, $current, $currentRate);
+            $hour = 1; 
+            $totalHours = 24; 
+
+            if (is_numeric($voltage) && is_numeric($current) && is_numeric($currentRate)) {
+                $power = calculatePower($voltage, $current);
+                $rate = $currentRate * 0.01; 
+
+                echo "<br>Power: " . number_format($power, 5) . " kW";
+                echo "<br>Rate: " . number_format($rate, 3) . " RM";
+
+                echo "<table class='table'>";
+                echo "<thead>";
+                echo "<tr><th>#</th><th>Hour</th><th>Energy (kWh)</th><th>Total Charge (RM)</th></tr>";
+                echo "</thead>";
+                echo "<tbody>";
+
+                for ($i = 1; $i <= $totalHours; $i++) {
+                    $energy = calculateEnergy($power, $i);
+                    $totalCharge = calculateTotalCharge($energy, $currentRate);
+
+                    echo "<tr>";
+                    echo "<td>$i</td>";
+                    echo "<td>$i</td>";
+                    echo "<td>" . number_format($energy, 5) . "</td>";
+                    echo "<td>" . number_format($totalCharge, 2) . "</td>";
+                    echo "</tr>";
+                }
+
+                echo "</tbody>";
+                echo "</table>";
+            } else {
+                echo "Invalid input. Please enter numeric values.";
+            }
         }
-
-        
         ?>
+    </div>
 </body>
 </html>
